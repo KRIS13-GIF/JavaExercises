e8279a0c9050b183f34bbaf7b167b475291fe99b
KRIS13-GIF <bekakris@yahoo.com>
2023-06-08 10:14:10 +0200

    day3  commit3

--- src/Day3/ex4.java
+++ src/Day3/ex4.java
@@ -5,19 +5,25 @@ public class ex4 {
         int [] myArray={21, 17, 3, 10, 9,20,90,8,2};
 
         int min=myArray[0];
+        int i_max=0;
+        int i_min=0;
         int max=myArray[0];
 
         for (int i=0; i<myArray.length; i++){
             if (myArray[i]>max){
                 max=myArray[i];
+                i_max=i;
             }
             else if (myArray[i]<min){
                 min=myArray[i];
+                i_min=i;
             }
         }
 
         System.out.println(max);
+        System.out.println("Index of max is: "+ i_max);
         System.out.println(min);
+        System.out.println("Index of min is: "+ i_min);
 
 
     }
